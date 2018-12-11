48113a7215a393b7c35af800e36301f44e21f318
Bertram <bjs70018@gmail.com>
2018-12-11 23:56:06 +0100

    first Try to get network response form movie db not working jet

--- app/src/main/java/com/pinschaneer/bertram/popularmovies/MainActivity.java
+++ app/src/main/java/com/pinschaneer/bertram/popularmovies/MainActivity.java
@@ -1,13 +1,30 @@
 package com.pinschaneer.bertram.popularmovies;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.pinschaneer.bertram.popularmovies.utilities.NetworkUtils;
+
+import java.io.IOException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView mMovieDisplayTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mMovieDisplayTextView = findViewById(R.id.tv_movie_display);
+        URL movieDbUrl = NetworkUtils.buildUrl("popular");
+        try {
+            String response = NetworkUtils.getResponseFromHttpUrl(movieDbUrl);
+            if (response != null) {
+                mMovieDisplayTextView.setText(response);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- app/src/main/java/com/pinschaneer/bertram/popularmovies/utilities/NetworkUtils.java
+++ app/src/main/java/com/pinschaneer/bertram/popularmovies/utilities/NetworkUtils.java
@@ -1,7 +1,69 @@
 package com.pinschaneer.bertram.popularmovies.utilities;
 
+import android.net.Uri;
+import android.util.Log;
+
+import java.io.EOFException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+
 public class NetworkUtils {
+
     private static final String TAG = NetworkUtils.class.getSimpleName();
 
+    final static String API_KEY_PARAM = "api_key";
+    final static String LANGUAGE_PARAM = "language";
+    final static String PAGE_PARAM = "page";
+    // TODO Add API Key
+    private static final String API_KEY = "";
+    private static final String MOVIE_DB_BASE_URL = "https://api.themoviedb.org/3/movie";
 
+    public static URL buildUrl(String movieCommand) {
+        String baseUrl = MOVIE_DB_BASE_URL + "/" + movieCommand;
+        Uri builtUri = Uri.parse(baseUrl).buildUpon()
+                .appendQueryParameter(API_KEY_PARAM, API_KEY)
+                .appendQueryParameter(LANGUAGE_PARAM, "en-US")
+                .appendQueryParameter(PAGE_PARAM, "1")
+                .build();
+
+        URL url = null;
+        try {
+            url = new URL(builtUri.toString());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        Log.v(TAG, "Built URI " + url);
+
+        return url;
+
+    }
+
+    public static String getResponseFromHttpUrl(URL url) throws IOException {
+
+
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        try {
+            InputStream in = urlConnection.getInputStream();
+
+            Scanner scanner = new Scanner(in);
+            scanner.useDelimiter("\\A");
+
+            boolean hasInput = scanner.hasNext();
+            if (hasInput) {
+                return scanner.next();
+            } else {
+                return null;
+            }
+        } catch (EOFException e) {
+            e.printStackTrace();
+        } finally {
+            urlConnection.disconnect();
+        }
+        return null;
+    }
 }
