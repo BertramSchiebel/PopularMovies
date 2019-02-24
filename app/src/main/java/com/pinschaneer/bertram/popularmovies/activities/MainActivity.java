5749137c4457a744a11888dd1e6bd85190abbe1b
Bertram <bjs70018@gmail.com>
2019-02-24 22:28:40 +0100

    Refactoring ViewModel

--- app/src/main/java/com/pinschaneer/bertram/popularmovies/activities/MainActivity.java
+++ app/src/main/java/com/pinschaneer/bertram/popularmovies/activities/MainActivity.java
@@ -1,13 +1,13 @@
 package com.pinschaneer.bertram.popularmovies.activities;
 
-import android.annotation.SuppressLint;
+import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.content.res.Resources;
-import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -17,18 +17,13 @@ import android.widget.ArrayAdapter;
 import android.widget.ProgressBar;
 import android.widget.Spinner;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.pinschaneer.bertram.popularmovies.R;
 import com.pinschaneer.bertram.popularmovies.activities.ViewModel.MainViewModel;
-import com.pinschaneer.bertram.popularmovies.data.MovieDBPageResult;
 import com.pinschaneer.bertram.popularmovies.data.MovieDataEntry;
 import com.pinschaneer.bertram.popularmovies.data.MovieListAdapter;
-import com.pinschaneer.bertram.popularmovies.utilities.NetworkUtils;
 
-import java.io.IOException;
-import java.net.URL;
-import java.util.Locale;
+import java.util.ArrayList;
 
 
 /**
@@ -37,12 +32,11 @@ import java.util.Locale;
 public class MainActivity extends AppCompatActivity implements MovieListAdapter.MovieListAdapterOnClickHandler, AdapterView.OnItemSelectedListener {
 
     private RecyclerView mMovieListRecyclerView;
-
+    private MovieListAdapter movieListAdapter;
     private MainViewModel viewModel;
     private TextView mErrorMessageDisplay;
     private ProgressBar mLoadingIndicator;
     private Spinner mMovieQuerySpinner;
-    private AsyncTask<String, MovieDBPageResult, MovieDBPageResult> mFetchMovieDataTask;
 
     /**
      * Display a error message
@@ -83,9 +77,9 @@ public class MainActivity extends AppCompatActivity implements MovieListAdapter.
         mMovieListRecyclerView.setLayoutManager(layoutManager);
 
         viewModel = ViewModelProviders.of(this).get(MainViewModel.class);
-        viewModel.Init(this);
 
-        mMovieListRecyclerView.setAdapter(viewModel.getMovieListAdapter());
+        movieListAdapter = new MovieListAdapter(this);
+        mMovieListRecyclerView.setAdapter(movieListAdapter);
 
         mErrorMessageDisplay = findViewById(R.id.tv_error_message_display);
 
@@ -100,23 +94,13 @@ public class MainActivity extends AppCompatActivity implements MovieListAdapter.
         movieQuerySpinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
         mMovieQuerySpinner.setAdapter(movieQuerySpinnerAdapter);
 
-        if (!viewModel.hasData()) {
-            mFetchMovieDataTask = new FetchMovieDataTask(this);
-        }
-    }
-
-
-    /**
-     * Load the movie data in an async task
-     */
-    private void loadMovieData() {
-        if (null != mFetchMovieDataTask) {
-            AsyncTask.Status status = mFetchMovieDataTask.getStatus();
-            if (status != AsyncTask.Status.RUNNING) {
-                viewModel.getMovieListAdapter().clearMovieData();
-                mFetchMovieDataTask = new FetchMovieDataTask(this).execute(viewModel.getCommand());
+        viewModel.getMovieDataEntries().observe(this, new Observer<ArrayList<MovieDataEntry>>()
+        {
+            @Override
+            public void onChanged(@Nullable ArrayList<MovieDataEntry> movieDataEntries) {
+                movieListAdapter.setMovieData(movieDataEntries);
             }
-        }
+        });
     }
 
 
@@ -157,7 +141,7 @@ public class MainActivity extends AppCompatActivity implements MovieListAdapter.
                 viewModel.setCommand(getString(R.string.most_popular_command));
         }
 
-        loadMovieData();
+        viewModel.loadMovieData();
     }
 
     /**
@@ -169,104 +153,4 @@ public class MainActivity extends AppCompatActivity implements MovieListAdapter.
         //auto generated stub
     }
 
-
-    /**
-     * Async task to get the requested movie data from the network
-     */
-    @SuppressLint("StaticFieldLeak")
-    private class FetchMovieDataTask extends AsyncTask<String, MovieDBPageResult, MovieDBPageResult> {
-
-        private final Context mContext;
-        private int mCurrentPageLoading;
-
-
-        /**
-         * constructor
-         * @param context the context of the task
-         */
-        private FetchMovieDataTask(Context context) {
-            this.mContext = context;
-        }
-
-        /**
-         * Adjustment before the execution
-         */
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            mMovieQuerySpinner.setEnabled(false);
-            mLoadingIndicator.setVisibility(View.VISIBLE);
-        }
-
-
-        /**
-         * The task that is done asynchronously
-         * @param searchParams thr parameter for the network request
-         * @return null
-         */
-        @Override
-        protected MovieDBPageResult doInBackground(String... searchParams) {
-            if (searchParams.length == 0) {
-                return null;
-            }
-            int totalPages = 5;
-            mCurrentPageLoading = 1;
-            //mLoadingPageIndicator.setProgress(mCurrentPageLoading);
-            MovieDBPageResult pageResult;
-            //mLoadingPageIndicator.setMax(totalPages);
-
-            do {
-                URL movieDbUrl = NetworkUtils.buildUrl(searchParams[0], Integer.toString(mCurrentPageLoading));
-                try {
-                    String response = NetworkUtils.getResponseFromHttpUrl(movieDbUrl);
-                    pageResult = MovieDBPageResult.parseMovieDbPageResult(response);
-                    publishProgress(pageResult);
-                    mCurrentPageLoading++;
-                } catch (IOException e) {
-                    e.printStackTrace();
-                    break;
-                }
-            } while (mCurrentPageLoading <= totalPages);
-
-            return null;
-        }
-
-
-        /**
-         * Displays the progress of the async task
-         * @param values the data of the search progres
-         */
-        @Override
-        protected void onProgressUpdate(MovieDBPageResult... values) {
-            super.onProgressUpdate(values);
-
-            if (values.length > 0) {
-                mLoadingIndicator.setVisibility(View.INVISIBLE);
-                viewModel.getMovieListAdapter().setMovieData(values[0].getResults());
-                showMovieResults();
-            }
-        }
-
-        /**
-         * Displays the summery of network request or an error message
-         * @param movieDBPageResult always null
-         */
-        @Override
-        protected void onPostExecute(MovieDBPageResult movieDBPageResult) {
-            super.onPostExecute(movieDBPageResult);
-
-            mMovieQuerySpinner.setEnabled(true);
-            mLoadingIndicator.setVisibility(View.INVISIBLE);
-            if (viewModel.getMovieListAdapter().getItemCount() == 0) {
-                mErrorMessageDisplay.setText(R.string.error_message);
-                showErrorMessage();
-            } else {
-
-                String msg = String.format(Locale.getDefault(), getString(R.string.message_loaded_data_set), viewModel.getMovieListAdapter().getItemCount());
-                Toast.makeText(mContext, msg, Toast.LENGTH_LONG)
-                        .show();
-
-            }
-        }
-    }
 }
--- app/src/main/java/com/pinschaneer/bertram/popularmovies/activities/ViewModel/MainViewModel.java
+++ app/src/main/java/com/pinschaneer/bertram/popularmovies/activities/ViewModel/MainViewModel.java
@@ -2,17 +2,36 @@ package com.pinschaneer.bertram.popularmovies.activities.ViewModel;
 
 import android.app.Application;
 import android.arch.lifecycle.AndroidViewModel;
+import android.arch.lifecycle.MutableLiveData;
+import android.os.AsyncTask;
 
+import com.pinschaneer.bertram.popularmovies.data.MovieDBPageResult;
+import com.pinschaneer.bertram.popularmovies.data.MovieDataEntry;
 import com.pinschaneer.bertram.popularmovies.data.MovieListAdapter;
+import com.pinschaneer.bertram.popularmovies.utilities.NetworkUtils;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
 
 public class MainViewModel extends AndroidViewModel
 {
-    private MovieListAdapter movieListAdapter;
+    //private MovieListAdapter movieListAdapter;
 
     private String command = "movie/popular";
 
+    private MutableLiveData<ArrayList<MovieDataEntry>> movieDataEntries;
+
     public MainViewModel(Application application) {
         super(application);
+        ArrayList<MovieDataEntry> movieList = new ArrayList<>();
+        movieDataEntries = new MutableLiveData();
+        movieDataEntries.setValue(movieList);
+        loadMovieData();
+    }
+
+    public MutableLiveData<ArrayList<MovieDataEntry>> getMovieDataEntries() {
+        return movieDataEntries;
     }
 
     public String getCommand() {
@@ -24,16 +43,65 @@ public class MainViewModel extends AndroidViewModel
     }
 
     public void Init(MovieListAdapter.MovieListAdapterOnClickHandler clickHandler) {
-        if (movieListAdapter == null) {
-            movieListAdapter = new MovieListAdapter(clickHandler);
+        //        if (movieListAdapter == null) {
+        //            movieListAdapter = new MovieListAdapter(clickHandler);
+        //        }
+
+
+    }
+
+
+    public void loadMovieData() {
+        movieDataEntries.getValue().clear();
+        new FetchMovieData().execute(command);
+    }
+
+    //    public MovieListAdapter getMovieListAdapter() {
+    //        return movieListAdapter;
+    //    }
+
+    public boolean hasData() {
+        return (movieDataEntries.getValue().size() > 0);
+    }
+
+    private class FetchMovieData extends AsyncTask<String, MovieDBPageResult, MovieDBPageResult>
+    {
+        @Override
+        protected MovieDBPageResult doInBackground(String... params) {
+            if (params.length == 0) {
+                return null;
+            }
+            int totalPages = 5;
+            int currentPageLoading = 1;
+            MovieDBPageResult pageResult;
+            do {
+                URL movieDbUrl = NetworkUtils.buildUrl(params[0], Integer.toString(currentPageLoading));
+                try {
+                    String response = NetworkUtils.getResponseFromHttpUrl(movieDbUrl);
+                    pageResult = MovieDBPageResult.parseMovieDbPageResult(response);
+                    publishProgress(pageResult);
+                    currentPageLoading++;
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                    break;
+                }
+            } while (currentPageLoading <= totalPages);
+
+            return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(MovieDBPageResult... values) {
+            super.onProgressUpdate(values);
+
+            if (values.length > 0) {
+                ArrayList<MovieDataEntry> entries = movieDataEntries.getValue();
+                entries.addAll(values[0].getResults());
+                movieDataEntries.postValue(entries);
+            }
         }
     }
 
-    public MovieListAdapter getMovieListAdapter() {
-        return movieListAdapter;
-    }
 
-    public boolean hasData() {
-        return (movieListAdapter.getItemCount() > 0);
-    }
 }
